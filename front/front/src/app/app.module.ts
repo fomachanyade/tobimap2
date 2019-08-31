dcc6ba171a4e8659c19143a0aacee7d8aa9a551e
高谷誉 <homare.takatani@cloudsteady.jp>
2019-09-01 04:12:36 +0900

    ADD: header

--- front/front/src/app/app.module.ts
+++ front/front/src/app/app.module.ts
@@ -4,13 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatNativeDateModule, MatInputModule, MatTabsModule, MatSidenavModule, MatIconModule } from '@angular/material';
+import { MatToolbarModule} from '@angular/material/toolbar';
+
+import { HeaderComponent } from './header/header.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
+    MatTabsModule, 
+    MatSidenavModule,
+    MatIconModule,
+
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
